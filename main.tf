6a56d116c7087f900d64df1988cf169373517b40
amossg <133963003+amossg@users.noreply.github.com>
2023-07-27 14:23:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-module "gke_auth" {
-  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  depends_on   = [module.gke]
-  project_id   = var.project_id
-  location     = module.gke.location
-  cluster_name = module.gke.name
-}
-
-resource "local_file" "kubeconfig" {
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "kubeconfig-${var.env_name}"
-}
 
 resource "google_sql_database_instance" "main" {
   name             = "main-instance"
