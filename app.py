9fe44aa5b1a2456df82b3457df7a25561a0fbfd6
amossg <133963003+amossg@users.noreply.github.com>
2023-07-27 16:29:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,56 +48,6 @@ conn.commit()
 cur.close()
 conn.close()
 
-from flask import Flask, render_template, request, redirect, url_for
-import psycopg2
-import subprocess
-
-app = Flask(__name__)
-
-import subprocess
-
-def get_public_ip():
-    # Run the Terraform command to get the public IP address
-    cmd = "terraform output -raw db_public_ip"
-    public_ip = subprocess.check_output(cmd, shell=True, text=True).strip()
-    return public_ip
-
-#if __name__ == "__main__":
-#    public_ip = get_public_ip()
-#    print(f"The public IP address is: {public_ip}")
-    # Now you can use the 'public_ip' variable in your Python code as needed.
-
-
-
-
-# Connect to the database
-conn = psycopg2.connect(
-    database="my-database",
-    user="me",
-    password="changeme", 
-    host=get_public_ip())
-  
-# create a cursor
-cur = conn.cursor()
-  
-# if you already have any table or not id doesnt matter this 
-# will create a products table for you.
-cur.execute(
-   '''CREATE TABLE IF NOT EXISTS products (id serial \
-    PRIMARY KEY, name varchar(100), price float);''')
-  
-# Insert some data into the table
-cur.execute(
-   '''INSERT INTO products (name, price) VALUES \
-  ('Apple', 1.99), ('Orange', 0.99), ('Banana', 0.59);''')
-  
-# commit the changes
-conn.commit()
-  
-# close the cursor and connection
-cur.close()
-conn.close()
-
 @app.route('/')
 def index():
     # Connect to the database
@@ -105,7 +55,7 @@ def index():
     database="postgres",
     user="postgres",
     password="1234",
-    host="34.116.216.64")
+    host=get_public_ip())
   
     # create a cursor
     cur = conn.cursor()
@@ -121,14 +71,14 @@ def index():
     conn.close()
   
     return render_template('index.html', data=data)
-
+    
 @app.route('/create', methods=['POST'])
 def create():
     conn = psycopg2.connect(
     database="postgres",
     user="postgres",
     password="1234",
-    host="34.116.216.64")
+    host=get_public_ip())
   
     cur = conn.cursor()
   
@@ -151,25 +101,44 @@ def create():
   
     return redirect(url_for('index'))
 
-@app.route('/create', methods=['POST'])
-def create():
+@app.route('/update', methods=['POST'])
+def update():
     conn = psycopg2.connect(
-    database="postgres",
-    user="postgres",
-    password="1234",
-    host="34.116.216.64")
+    database="my-database",
+    user="me",
+    password="cahangeme",
+    host=get_public_ip())
   
     cur = conn.cursor()
   
     # Get the data from the form
     name = request.form['name']
     price = request.form['price']
+    id = request.form['id']
   
-    # Insert the data into the table
+    # Update the data in the table
     cur.execute(
-        '''INSERT INTO products \
-        (name, price) VALUES (%s, %s)''',
-        (name, price))
+        '''UPDATE products SET name=%s,\
+        price=%s WHERE id=%s''', (name, price, id))
+  
+    # commit the changes
+    conn.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    conn = psycopg2.connect(
+    database="postgres",
+    user="postgres",
+    password="1234",
+    host=get_public_ip())
+    cur = conn.cursor()
+  
+    # Get the data from the form
+    id = request.form['id']
+  
+    # Delete the data from the table
+    cur.execute('''DELETE FROM products WHERE id=%s''', (id,))
   
     # commit the changes
     conn.commit()
@@ -179,10 +148,11 @@ def create():
     conn.close()
   
     return redirect(url_for('index'))
-
+  
+  
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0',  port=45612)
    #from waitress import serve
    #serve(app, port=45612)
-  
-  
+
+
