b7033a5a5d9631b5f879660ee2f90fcebf09282c
amossg <133963003+amossg@users.noreply.github.com>
2023-07-27 14:21:32 +0300

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,24 @@
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+   #   version = "4.8.0"
+    }
+  }
+}
+
+
+provider "google" {
+    project = "celtic-beacon-387519"
+    region = "europe-central2"
+    credentials = file("./key.json")
+    zone = "europe-central2-b"
+
+}
+
+
+provider "google-beta" {
+    project = "celtic-beacon-387519"
+    region = "europe-central2"
+    credentials = file("./key.json")
+    zone = "europe-central2-b"
+}
