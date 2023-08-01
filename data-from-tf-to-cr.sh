c7defe24eb239a3ba510c9b210987ac3f690eea2
amossg <133963003+amossg@users.noreply.github.com>
2023-08-01 12:34:29 +0300

    Create data-from-tf-to-cr.sh

---
+++ data-from-tf-to-cr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run Terraform output to get the values and store them in variables
+db_public_ip=$(terraform output db_public_ip)
+database_name=$(terraform output database_name)
+db_user=$(terraform output db_user)
+db_password=$(terraform output db_password)
+# Add more variables for other outputs as needed
+
+# Create the Kubernetes YAML file (e.g., configmap.yaml) with the variables
+cat <<EOF > custom-resource.yaml
+apiVersion: pgconn.flask.me/v1
+kind: PgConn
+metadata:
+  name: my-postgres-connection
+spec:
+  host: $db_public_ip
+  database: $database_name
+  username: $db_user
+  password: $db_password
+EOF
